829f97a3eb733b6e4d35e6e260db167f76ba1a51
gath <gatheesha.official@gmail.com>
2021-01-08 18:21:02 +0530

    basic movement
    movement
    with some prototype assests

---
+++ Scripts/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (int) var gravity = 400
+export (bool) var jumpable = true
+export (int) var jump_speed = -180
+
+var velocity = Vector2.ZERO
+
+func get_input():
+	velocity.x = 0
+	if Input.is_action_pressed("go_right"):
+		velocity.x += speed
+	if Input.is_action_pressed("go_left"):
+		velocity.x -= speed
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("jump") && jumpable == true:
+		if is_on_floor():
+			velocity.y = jump_speed
