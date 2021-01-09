3f3cb82e16206d5d21a6f9bafaa4790700e31150
Xuk0 <ticcytwitch@gmail.com>
2021-01-09 12:52:22 +0530

    gravity
    just add gravity variable

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -5,6 +5,8 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var posx
+var gravity = 10
+#hi hello everyone i currenlty a stupid!
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
